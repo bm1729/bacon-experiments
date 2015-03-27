77f7b7096c14597ff074419c36722746d63650ba
bm1729 <ben.mumford1729@gmail.com>
2015-03-27 10:28:57 +0000

    Removing state from the DOM

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -2,14 +2,17 @@ $(function() {
     
     var startClickStream = $('#start').asEventStream('click');
     
+    // TODO How can I get rid of the level bus?
     var levelBus = new Bacon.Bus();
     var levelProperty = levelBus.toProperty(1);
     
+    // When the level changes - update the level text and empty the game board
     levelProperty.onValue(function(level) {
             $("#level").text(level);
             $('#gameBoard').empty();
         });
     
+    // When the level changes - generate the new colours
     var coloursProperty = levelProperty.map(function(level) {
             var coloursLength = level * 2;
             var newColours = _.times(coloursLength, _.constant("blue"));
@@ -24,23 +27,39 @@ $(function() {
             return newColours;
         });
     
-    var startStream = coloursProperty.sampledBy(startClickStream);
-    startStream.onValue(function (colours) {
-            $.each(colours, function(index, value) {
-                $('#gameBoard').append('<div class="col-xs-3 ' + colours[index] + '" id="' + index + '"></div>')
-            });
-        });
-    startStream.delay(2000)
-        .onValue(function() {
-            $('#gameBoard div').css('background-color', 'grey');
+    // Whether or not we are showing the true tiles' colour
+    var showingTilesProperty = startClickStream.flatMapLatest(function(colours) {
+            return Bacon.once(true).merge(Bacon.later(2000, false));
+        })
+        .toProperty(false);
+        
+    // Set the tile colours as appropriate
+    Bacon.combineTemplate(
+        {
+            colours: coloursProperty,
+            showingTiles: showingTilesProperty
+        })
+        .onValue(function(value) {
+            if (value.showingTiles) {
+                $.each(value.colours, function(index, value) {
+                    $('#gameBoard').append('<div class="col-xs-3" id="' + index + '"></div>');
+                });
+                $('#gameBoard div').each(function(index, element) {
+                    $(element).css('background-color', value.colours[ element.id]);
+                });
+            } else {
+                $('#gameBoard div').css('background-color', 'grey');
+            }
         });
     
+    // When someone clicks on the game board and the tiles have been hidden
+    // TODO Don't store state in the DOM
     var gameClickStream =  $('#gameBoard').asEventStream('click')
         .filter(function(event) { return event.target.id !== 'gameBoard'; })
-        .filter(function(event) {
-            return $(event.target).css('background-color') === 'rgb(128, 128, 128)';
-        });
+        .filter(showingTilesProperty.not());
     
+    // Show the red tiles as they are uncovered - go to next level when all red tiles are found or back to level one if they make a mistake
+    // TODO Don't store state in the DOM
     var gameTemplate = Bacon.combineTemplate({
             colours: coloursProperty,
             level: levelProperty
@@ -68,25 +87,4 @@ $(function() {
                 levelBus.push(1);
             }
         });
-    
-
-    // gameStream.onValue(function(eventObject) {
-    //     var tileId = eventObject.gameClick.target.id;
-    //     var tileColor = eventObject.colours[tileId];
-    //     if (tileColor === 'red') {
-    //         $('#' + tileId).css('background-color', 'red');
-    //         var redsExposed = $('#gameBoard div').filter(function() {
-    //             return $(this).css('background-color') === 'rgb(255, 0, 0)';
-    //         }).length;
-    //         if (redsExposed === eventObject.level) {
-    //             alert("You win!");
-    //             levelBus.push(eventObject.level + 1);
-    //         }
-    //     } else {
-    //         alert("You lose!");
-    //         levelBus.push(1);
-    //     }
-    // });
-    
-    // var showingTiles = startStream.toProperty(true);
 });
